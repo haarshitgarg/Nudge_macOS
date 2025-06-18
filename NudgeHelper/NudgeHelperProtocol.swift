7b2fc69d7254cafcfb07c1a3c46dcd3d9d8beaad
haarshitgarg <59084710+haarshitgarg@users.noreply.github.com>
2025-06-18 18:43:39 +0530

    Adding the following changes:
    - modified info.plist to add information about accessibility usage
    - Added xpcservice target which is not sandboxed

---
+++ NudgeHelper/NudgeHelperProtocol.swift
@@ -0,0 +1,35 @@
+//
+//  NudgeHelperProtocol.swift
+//  NudgeHelper
+//
+//  Created by Harshit Garg on 18/06/25.
+//
+
+import Foundation
+
+/// The protocol that this service will vend as its API. This protocol will also need to be visible to the process hosting the service.
+@objc protocol NudgeHelperProtocol {
+    
+    /// Replace the API of this protocol with an API appropriate to the service you are vending.
+    func performCalculation(firstNumber: Int, secondNumber: Int, with reply: @escaping (Int) -> Void)
+}
+
+/*
+ To use the service from an application or other process, use NSXPCConnection to establish a connection to the service by doing something like this:
+
+     connectionToService = NSXPCConnection(serviceName: "Harshit.NudgeHelper")
+     connectionToService.remoteObjectInterface = NSXPCInterface(with: (any NudgeHelperProtocol).self)
+     connectionToService.resume()
+
+ Once you have a connection to the service, you can use it like this:
+
+     if let proxy = connectionToService.remoteObjectProxy as? NudgeHelperProtocol {
+         proxy.performCalculation(firstNumber: 23, secondNumber: 19) { result in
+             NSLog("Result of calculation is: \(result)")
+         }
+     }
+
+ And, when you are finished with the service, clean up the connection like this:
+
+     connectionToService.invalidate()
+*/
